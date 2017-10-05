b33fe49a0a7b3fcea14685c57ff44ad00a573546
gokanb <gokan.birdal@hititbet.com>
2017-10-05 15:10:13 +0300

    edit

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+  providers: [ProductService]
+})
+export class ProductComponent implements OnInit {
+
+  products: Product[];
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(p => {
+      this.products = p;
+    });
+  }
+
+}

db4c8b497f14fb7018acae4e84aac55e2fdce4d7
gokanb <gokan.birdal@hititbet.com>
2017-11-01 16:30:34 +0200

    shipping-detail

--- src/app/shipping-detail/shipping-detail.component.ts
+++ src/app/shipping-detail/shipping-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ShippingDetail } from './shipping-detail';
 
 @Component({
-  selector: 'app-shipping-detail',
-  templateUrl: './shipping-detail.component.html',
-  styleUrls: ['./shipping-detail.component.css']
+	selector: 'app-shipping-detail',
+	templateUrl: './shipping-detail.component.html',
+	styleUrls: ['./shipping-detail.component.css']
 })
 export class ShippingDetailComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
+	cities = [];
+	model: ShippingDetail = new ShippingDetail('', '', true, -1);
+	ngOnInit() {
+		this.cities.push(
+			{ id: 1, name: "Ankara" },
+			{ id: 2, name: "İstanbul" },
+			{ id: 3, name: "Londra" }
+		);
+	}
 
-  ngOnInit() {
-  }
+	checkout(form: NgForm) {
+
+	}
 
 }
